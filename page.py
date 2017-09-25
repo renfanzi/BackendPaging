0f18a1dec25e57d102320436c96ca2c95edac966
renfanzi <renfanzi@users.noreply.github.com>
2017-09-25 11:59:31 +0800

    Update page.py

--- page.py
+++ page.py
@@ -87,7 +87,47 @@ class Pagination:
 
         return str_page
 
+class MyPagesAndPageDatas():
+    """
+    from common.util.MyPaging import Pagination
 
+    res = MyPagesAndPageDatas()
+    dataCount = res.SelectItemPagesCountModel("2017091710094680349524135490")
+    obj = Pagination(dataCount, current_page=52)
+    start = obj.start
+    end = obj.end
+    data = res.SelectItemPagesDataModel(start, end, obj.appear_page)
+    print(len(data))
+
+    res.close()
+    """
+    def __init__(self, libname="notdbMysql"):
+        self.libname = libname
+        self.res = MyPymysql(self.libname)
+
+    def SelectItemPagesCountModel(self, QuesID):
+        selectBaseTableSql = "select DataTableID, DataTableName, DatabaseName from db_metadata.meta_data_table WHERE `QuesID`='{}' AND DataTableStatus=1;".format(
+            QuesID)
+        self.baseTableData = self.res.selectone_sql(
+            selectBaseTableSql)  # {'DataTableName': '', 'DataTableID': '', 'DatabaseName': ''}
+
+        dataSql = """select count(1) as count from {}.{}""".format(self.baseTableData["DatabaseName"],
+                                                                   self.baseTableData["DataTableName"])
+
+        result = self.res.selectone_sql(dataSql)
+        return result["count"]
+
+    def SelectItemPagesDataModel(self, start, end, appear_page):
+        sql = """select `StartTime`, `EndTime`, `NominalTime`, `SpaceName`, `Topic`, `Index`, `DataValue`, `DataDescription` from {}.{} limit {}, {};""".format(
+            self.baseTableData["DatabaseName"],
+            self.baseTableData["DataTableName"],
+            start,
+            appear_page)
+        result = self.res.selectall_sql(sql)
+        return result
+
+    def close(self):
+        self.res.close()
 if __name__ == '__main__':
     obj = Pagination(1001, 1)
     print(obj.start)
